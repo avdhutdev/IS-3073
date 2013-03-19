006fdb84fc6518b41e306fb6153d2879a4e2cd4f
John-William Trenholm <jw@xomar.com>
2013-03-19 16:45:30 -0500

    inclass 7
    lists and stuffz

--- inClass7/inClass7/Form1.Designer.vb
+++ inClass7/inClass7/Form1.Designer.vb
@@ -22,9 +22,180 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ListBox1 = New System.Windows.Forms.ListBox
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox
+        Me.inputTextBox = New System.Windows.Forms.TextBox
+        Me.addButton = New System.Windows.Forms.Button
+        Me.insertButton = New System.Windows.Forms.Button
+        Me.positionBox = New System.Windows.Forms.TextBox
+        Me.countButton = New System.Windows.Forms.Button
+        Me.changeButton = New System.Windows.Forms.Button
+        Me.changeTextBox = New System.Windows.Forms.TextBox
+        Me.removeAtbutton = New System.Windows.Forms.Button
+        Me.removeTextBox = New System.Windows.Forms.TextBox
+        Me.removePizza = New System.Windows.Forms.Button
+        Me.removeStringTextBox = New System.Windows.Forms.TextBox
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Items.AddRange(New Object() {"Beerz", "Chicken Wings", "Hamburgers", "Hotdogs", "Pizza"})
+        Me.ListBox1.Location = New System.Drawing.Point(150, 68)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox1.TabIndex = 0
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Beerz", "Chicken Wings", "Hamburgers", "Hotdogs", "Pizza"})
+        Me.ComboBox1.Location = New System.Drawing.Point(76, 41)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.Sorted = True
+        Me.ComboBox1.TabIndex = 1
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"Beerz", "Chicken Wings", "Hamburgers", "Hotdogs", "Ice Cream", "Pizza"})
+        Me.ComboBox2.Location = New System.Drawing.Point(23, 68)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(100, 124)
+        Me.ComboBox2.Sorted = True
+        Me.ComboBox2.TabIndex = 2
+        '
+        'inputTextBox
+        '
+        Me.inputTextBox.Location = New System.Drawing.Point(150, 159)
+        Me.inputTextBox.Name = "inputTextBox"
+        Me.inputTextBox.Size = New System.Drawing.Size(120, 20)
+        Me.inputTextBox.TabIndex = 3
+        '
+        'addButton
+        '
+        Me.addButton.Location = New System.Drawing.Point(23, 198)
+        Me.addButton.Name = "addButton"
+        Me.addButton.Size = New System.Drawing.Size(75, 23)
+        Me.addButton.TabIndex = 4
+        Me.addButton.Text = "Add"
+        Me.addButton.UseVisualStyleBackColor = True
+        '
+        'insertButton
+        '
+        Me.insertButton.Location = New System.Drawing.Point(23, 236)
+        Me.insertButton.Name = "insertButton"
+        Me.insertButton.Size = New System.Drawing.Size(75, 23)
+        Me.insertButton.TabIndex = 5
+        Me.insertButton.Text = "Insert"
+        Me.insertButton.UseVisualStyleBackColor = True
+        '
+        'positionBox
+        '
+        Me.positionBox.Location = New System.Drawing.Point(133, 239)
+        Me.positionBox.Name = "positionBox"
+        Me.positionBox.Size = New System.Drawing.Size(100, 20)
+        Me.positionBox.TabIndex = 6
+        '
+        'countButton
+        '
+        Me.countButton.Location = New System.Drawing.Point(23, 273)
+        Me.countButton.Name = "countButton"
+        Me.countButton.Size = New System.Drawing.Size(75, 23)
+        Me.countButton.TabIndex = 7
+        Me.countButton.Text = "Count"
+        Me.countButton.UseVisualStyleBackColor = True
+        '
+        'changeButton
+        '
+        Me.changeButton.Location = New System.Drawing.Point(23, 312)
+        Me.changeButton.Name = "changeButton"
+        Me.changeButton.Size = New System.Drawing.Size(75, 23)
+        Me.changeButton.TabIndex = 8
+        Me.changeButton.Text = "Change"
+        Me.changeButton.UseVisualStyleBackColor = True
+        '
+        'changeTextBox
+        '
+        Me.changeTextBox.Location = New System.Drawing.Point(133, 312)
+        Me.changeTextBox.Name = "changeTextBox"
+        Me.changeTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.changeTextBox.TabIndex = 9
+        '
+        'removeAtbutton
+        '
+        Me.removeAtbutton.Location = New System.Drawing.Point(23, 359)
+        Me.removeAtbutton.Name = "removeAtbutton"
+        Me.removeAtbutton.Size = New System.Drawing.Size(75, 23)
+        Me.removeAtbutton.TabIndex = 10
+        Me.removeAtbutton.Text = "Remove"
+        Me.removeAtbutton.UseVisualStyleBackColor = True
+        '
+        'removeTextBox
+        '
+        Me.removeTextBox.Location = New System.Drawing.Point(133, 359)
+        Me.removeTextBox.Name = "removeTextBox"
+        Me.removeTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.removeTextBox.TabIndex = 11
+        '
+        'removePizza
+        '
+        Me.removePizza.Location = New System.Drawing.Point(23, 404)
+        Me.removePizza.Name = "removePizza"
+        Me.removePizza.Size = New System.Drawing.Size(75, 23)
+        Me.removePizza.TabIndex = 12
+        Me.removePizza.Text = "removePizza"
+        Me.removePizza.UseVisualStyleBackColor = True
+        '
+        'removeStringTextBox
+        '
+        Me.removeStringTextBox.Location = New System.Drawing.Point(133, 404)
+        Me.removeStringTextBox.Name = "removeStringTextBox"
+        Me.removeStringTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.removeStringTextBox.TabIndex = 13
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(282, 451)
+        Me.Controls.Add(Me.removeStringTextBox)
+        Me.Controls.Add(Me.removePizza)
+        Me.Controls.Add(Me.removeTextBox)
+        Me.Controls.Add(Me.removeAtbutton)
+        Me.Controls.Add(Me.changeTextBox)
+        Me.Controls.Add(Me.changeButton)
+        Me.Controls.Add(Me.countButton)
+        Me.Controls.Add(Me.positionBox)
+        Me.Controls.Add(Me.insertButton)
+        Me.Controls.Add(Me.addButton)
+        Me.Controls.Add(Me.inputTextBox)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents inputTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents addButton As System.Windows.Forms.Button
+    Friend WithEvents insertButton As System.Windows.Forms.Button
+    Friend WithEvents positionBox As System.Windows.Forms.TextBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents countButton As System.Windows.Forms.Button
+    Friend WithEvents changeButton As System.Windows.Forms.Button
+    Friend WithEvents changeTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents removeAtbutton As System.Windows.Forms.Button
+    Friend WithEvents removeTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents removePizza As System.Windows.Forms.Button
+    Friend WithEvents removeStringTextBox As System.Windows.Forms.TextBox
 
 End Class
--- inClass7/inClass7/Form1.vb
+++ inClass7/inClass7/Form1.vb
@@ -1,3 +1,36 @@
 ﻿Public Class Form1
 
+    Private Sub addButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles addButton.Click
+        ListBox1.Items.Add("Ice Cream")
+        ComboBox2.Items.Add(ComboBox2.Text)
+    End Sub
+
+    Private Sub insertButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles insertButton.Click
+        ListBox1.Items.Insert(Integer.Parse(positionBox.Text - 1), "Your mom's Sandwhiches")
+
+    End Sub
+
+    Private Sub countButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles countButton.Click
+        MessageBox.Show(ListBox1.Items.Count(), "ListBox1 count", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+
+    End Sub
+
+    Private Sub changeButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles changeButton.Click
+        'ListBox1.Items.IndexOf(Integer.Parse(changeTextBox.Text))
+        'MessageBox.Show(ListBox1.Items.IndexOf(Integer.Parse(changeTextBox.Text)), "ListBox1 count", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+
+
+    End Sub
+
+    Private Sub removeAtbutton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles removeAtbutton.Click
+        If ListBox1.Items.Count < (Integer.Parse(removeTextBox.Text - 1)) Then
+            MessageBox.Show("Please select a valid item", "ListBox1 count", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+        Else
+            ListBox1.Items.RemoveAt(Integer.Parse(removeTextBox.Text - 1))
+        End If
+    End Sub
+
+    Private Sub removePizza_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles removePizza.Click
+        ListBox1.Items.Remove(removeStringTextBox.Text)
+    End Sub
 End Class
