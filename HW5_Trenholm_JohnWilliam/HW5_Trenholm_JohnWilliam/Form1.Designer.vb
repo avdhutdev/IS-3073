5819ab4cfdd88acc7e48b303435d96bb49b397b6
jw@xomar.com <jw@xomar.com>
2013-03-28 02:12:54 -0500

    hw5

---
+++ HW5_Trenholm_JohnWilliam/HW5_Trenholm_JohnWilliam/Form1.Designer.vb
@@ -0,0 +1,319 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.PrintSummaryToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.PrintYachtTypeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem
+        Me.ClearForNextToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator
+        Me.AddYachtTypeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.RemoveYachtTypeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.DisplayYachtTypeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.okButton = New System.Windows.Forms.Button
+        Me.clearButton = New System.Windows.Forms.Button
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.responsiblePartyBox = New System.Windows.Forms.TextBox
+        Me.hoursCharteredBox = New System.Windows.Forms.TextBox
+        Me.totalChargesBox = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox
+        Me.MenuStrip1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(728, 28)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PrintSummaryToolStripMenuItem, Me.PrintYachtTypeToolStripMenuItem, Me.ToolStripSeparator1, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(44, 24)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'PrintSummaryToolStripMenuItem
+        '
+        Me.PrintSummaryToolStripMenuItem.Name = "PrintSummaryToolStripMenuItem"
+        Me.PrintSummaryToolStripMenuItem.Size = New System.Drawing.Size(184, 24)
+        Me.PrintSummaryToolStripMenuItem.Text = "Print &Summary"
+        '
+        'PrintYachtTypeToolStripMenuItem
+        '
+        Me.PrintYachtTypeToolStripMenuItem.Name = "PrintYachtTypeToolStripMenuItem"
+        Me.PrintYachtTypeToolStripMenuItem.Size = New System.Drawing.Size(184, 24)
+        Me.PrintYachtTypeToolStripMenuItem.Text = "Print &Yacht Type"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(181, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(184, 24)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClearForNextToolStripMenuItem, Me.ToolStripSeparator2, Me.AddYachtTypeToolStripMenuItem, Me.RemoveYachtTypeToolStripMenuItem, Me.DisplayYachtTypeToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(47, 24)
+        Me.EditToolStripMenuItem.Text = "&Edit"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem2})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(53, 24)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(152, 24)
+        Me.ToolStripMenuItem2.Text = "&About"
+        '
+        'ClearForNextToolStripMenuItem
+        '
+        Me.ClearForNextToolStripMenuItem.Name = "ClearForNextToolStripMenuItem"
+        Me.ClearForNextToolStripMenuItem.Size = New System.Drawing.Size(224, 24)
+        Me.ClearForNextToolStripMenuItem.Text = "&Clear For Next Charter"
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(221, 6)
+        '
+        'AddYachtTypeToolStripMenuItem
+        '
+        Me.AddYachtTypeToolStripMenuItem.Name = "AddYachtTypeToolStripMenuItem"
+        Me.AddYachtTypeToolStripMenuItem.Size = New System.Drawing.Size(224, 24)
+        Me.AddYachtTypeToolStripMenuItem.Text = "&Add Yacht Type"
+        '
+        'RemoveYachtTypeToolStripMenuItem
+        '
+        Me.RemoveYachtTypeToolStripMenuItem.Name = "RemoveYachtTypeToolStripMenuItem"
+        Me.RemoveYachtTypeToolStripMenuItem.Size = New System.Drawing.Size(224, 24)
+        Me.RemoveYachtTypeToolStripMenuItem.Text = "&Remove Yacht Type"
+        '
+        'DisplayYachtTypeToolStripMenuItem
+        '
+        Me.DisplayYachtTypeToolStripMenuItem.Name = "DisplayYachtTypeToolStripMenuItem"
+        Me.DisplayYachtTypeToolStripMenuItem.Size = New System.Drawing.Size(224, 24)
+        Me.DisplayYachtTypeToolStripMenuItem.Text = "&Display Yacht Type"
+        '
+        'okButton
+        '
+        Me.okButton.Location = New System.Drawing.Point(422, 301)
+        Me.okButton.Name = "okButton"
+        Me.okButton.Size = New System.Drawing.Size(75, 23)
+        Me.okButton.TabIndex = 9
+        Me.okButton.Text = "&OK"
+        Me.okButton.UseVisualStyleBackColor = True
+        '
+        'clearButton
+        '
+        Me.clearButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.clearButton.Location = New System.Drawing.Point(534, 301)
+        Me.clearButton.Name = "clearButton"
+        Me.clearButton.Size = New System.Drawing.Size(75, 23)
+        Me.clearButton.TabIndex = 0
+        Me.clearButton.Text = "&Clear"
+        Me.clearButton.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.ComboBox2)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(422, 70)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(264, 193)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Type && Length"
+        '
+        'responsiblePartyBox
+        '
+        Me.responsiblePartyBox.Location = New System.Drawing.Point(196, 70)
+        Me.responsiblePartyBox.Name = "responsiblePartyBox"
+        Me.responsiblePartyBox.Size = New System.Drawing.Size(200, 22)
+        Me.responsiblePartyBox.TabIndex = 2
+        '
+        'hoursCharteredBox
+        '
+        Me.hoursCharteredBox.Location = New System.Drawing.Point(196, 135)
+        Me.hoursCharteredBox.Name = "hoursCharteredBox"
+        Me.hoursCharteredBox.Size = New System.Drawing.Size(100, 22)
+        Me.hoursCharteredBox.TabIndex = 4
+        '
+        'totalChargesBox
+        '
+        Me.totalChargesBox.Location = New System.Drawing.Point(196, 222)
+        Me.totalChargesBox.Name = "totalChargesBox"
+        Me.totalChargesBox.ReadOnly = True
+        Me.totalChargesBox.Size = New System.Drawing.Size(100, 22)
+        Me.totalChargesBox.TabIndex = 8
+        Me.totalChargesBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 222)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(101, 17)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Total Charges:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 135)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(117, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Hou&rs Chartered:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(35, 70)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(127, 17)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Responsible &Party:"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"C & C", "Catalina", "Coronado", "Excalibur", "Hans Christian", "Hobie", "Ranger", "Wavelength"})
+        Me.ComboBox1.Location = New System.Drawing.Point(21, 56)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(215, 24)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(18, 24)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(84, 17)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Yacht &Type:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(23, 92)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(92, 17)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = "Yacht &Length"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"22", "24", "30", "32", "36", "38", "45"})
+        Me.ComboBox2.Location = New System.Drawing.Point(26, 124)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(210, 24)
+        Me.ComboBox2.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.okButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.clearButton
+        Me.ClientSize = New System.Drawing.Size(728, 407)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.totalChargesBox)
+        Me.Controls.Add(Me.hoursCharteredBox)
+        Me.Controls.Add(Me.responsiblePartyBox)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.clearButton)
+        Me.Controls.Add(Me.okButton)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Yacht Charters"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PrintSummaryToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PrintYachtTypeToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ClearForNextToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator2 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents AddYachtTypeToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents RemoveYachtTypeToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents DisplayYachtTypeToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents okButton As System.Windows.Forms.Button
+    Friend WithEvents clearButton As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents responsiblePartyBox As System.Windows.Forms.TextBox
+    Friend WithEvents hoursCharteredBox As System.Windows.Forms.TextBox
+    Friend WithEvents totalChargesBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+
+End Class
---
+++ HW5_Trenholm_JohnWilliam/HW5_Trenholm_JohnWilliam/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub ExitToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub clearButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles clearButton.Click
+        responsiblePartyBox.Clear()
+        hoursCharteredBox.Clear()
+        totalChargesBox.Clear()
+
+    End Sub
+
+    Private Sub ToolStripMenuItem2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripMenuItem2.Click
+        AboutBox1.Show()
+
+    End Sub
+End Class
